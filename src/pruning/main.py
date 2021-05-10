2e127c04d948a1a41cc076f10df31793b50e3021
Arvind Subramaniam <37202614+ArvindSubramaniam@users.noreply.github.com>
2021-05-09 21:16:37 -0400

    Update main.py

--- src/pruning/main.py
+++ src/pruning/main.py
@@ -10,6 +10,42 @@ from torchvision import transforms
 import numpy as np
 import copy
 import os
+from models import *
+
+def network_init():
+    
+    
+  model = VGG16([ 64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M',
+       512, 512, 512, 'M'
+   ])
+  # model = resnet34().cuda()
+  # model = AlexNet()
+  optimiser = optim.SGD( model.parameters(), lr=lr, momentum=0.9, weight_decay=weight_decay)
+  scheduler = optim.lr_scheduler.StepLR(optimiser, lr_decay_interval, gamma=0.1)
+
+
+  train_transform = transforms.Compose([
+      transforms.RandomCrop(32, padding=4),
+      transforms.RandomHorizontalFlip(),
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465),
+                          (0.2023, 0.1994, 0.2010)),
+  ])
+
+  test_transform = transforms.Compose([
+      transforms.RandomCrop(32, padding=4),
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465),
+                          (0.2023, 0.1994, 0.2010)),
+  ])
+
+  train_dataset = CIFAR10('_dataset', True, train_transform, download=True)
+  test_dataset = CIFAR10('_dataset', False, test_transform, download=False)
+
+  train_loader = DataLoader( train_dataset, batch_size, shuffle=True, num_workers=2, pin_memory=True)
+  val_loader = DataLoader( test_dataset, batch_size, shuffle=False, num_workers=2, pin_memory=True)
+
+  return model, optimiser, scheduler, train_loader, val_loader
 
 
 model, optimiser, lr_scheduler, train_loader, val_loader = network_init()
